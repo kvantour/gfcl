50ec1d10d9f623ba0ff5fa24638bf6f9028f2e6f
kvantour <15147688+kvantour@users.noreply.github.com>
2017-08-30 23:55:32 +0200

    [coding_style] introduced new coding style:   * subroutines and functions not public end with "___"   * private procedure names, end with "__" [iterators]   * replaced operator(+) and operator(-) with overloaded next and prev   * introduced advance subroutine

--- src/gfl_iterators.f90
+++ src/gfl_iterators.f90
@@ -7,13 +7,14 @@ MODULE gfcl_iterators
   TYPE, ABSTRACT :: forward_iterator
    CONTAINS
      PRIVATE
-     PROCEDURE(op1_fi), DEFERRED, PASS :: equal
-     PROCEDURE(op1_fi), DEFERRED, PASS :: notequal
+     PROCEDURE(op1_fi), DEFERRED, PASS :: equal__
+     PROCEDURE(op1_fi), DEFERRED, PASS :: notequal__
+     PROCEDURE(op2_fi), DEFERRED, PASS :: next__
 
-     PROCEDURE(op2_fi), PUBLIC, DEFERRED, PASS :: next
+     GENERIC, PUBLIC :: OPERATOR(==) => equal__
+     GENERIC, PUBLIC :: OPERATOR(/=) => notequal__
 
-     GENERIC, PUBLIC :: OPERATOR(==) => equal
-     GENERIC, PUBLIC :: OPERATOR(/=) => notequal
+     GENERIC, PUBLIC :: next => next__
   END TYPE forward_iterator
 
   ! Bidirectional iterators are iterators that can be used to access
@@ -22,10 +23,10 @@ MODULE gfcl_iterators
 
   TYPE, EXTENDS(forward_iterator), ABSTRACT :: bidirectional_iterator
    CONTAINS
-!     PROCEDURE(op1_bi), DEFERRED, PASS(c_this) :: equal
-!     PROCEDURE(op1_bi), DEFERRED, PASS(c_this) :: notequal
      PRIVATE
-     PROCEDURE(op2_bi), PUBLIC, DEFERRED, PASS :: prev
+     PROCEDURE(op2_bi), DEFERRED, PASS :: prev__
+
+     GENERIC, PUBLIC :: prev => prev__
   END TYPE bidirectional_iterator
 
   ! Random-access iterators are iterators that can be used to access
@@ -39,21 +40,23 @@ MODULE gfcl_iterators
 
    CONTAINS
      PRIVATE
-     PROCEDURE(op1_ri), DEFERRED, PASS(c_this) :: add_integer_post
-     PROCEDURE(op1_ri), DEFERRED, PASS(c_this) :: subtract_integer_post
+     PROCEDURE(op1_ri), DEFERRED, PASS(c_this) :: add_integer__
+     PROCEDURE(op1_ri), DEFERRED, PASS(c_this) :: subtract_integer__
      PROCEDURE(op2_ri), DEFERRED, PASS(c_this) :: add_integer_pre
      PROCEDURE(op3_ri), DEFERRED, PASS(c_this) :: subtract_iterator
-     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: lt
-     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: gt
-     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: gte
-     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: lte
+     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: lower_than__
+     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: lower_than_equal__
+     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: greater_than__
+     PROCEDURE(op4_ri), DEFERRED, PASS(c_this) :: greater_than_equal__
 
-     GENERIC, PUBLIC :: OPERATOR(+)  => add_integer_post, add_integer_pre
-     GENERIC, PUBLIC :: OPERATOR(-)  => subtract_integer_post, subtract_iterator
-     GENERIC, PUBLIC :: OPERATOR(<)  => lt
-     GENERIC, PUBLIC :: OPERATOR(<=) => lte
-     GENERIC, PUBLIC :: OPERATOR(>)  => gt
-     GENERIC, PUBLIC :: OPERATOR(>=) => gte
+     GENERIC, PUBLIC :: OPERATOR(<)  => lower_than__
+     GENERIC, PUBLIC :: OPERATOR(<=) => lower_than_equal__
+     GENERIC, PUBLIC :: OPERATOR(>)  => greater_than__
+     GENERIC, PUBLIC :: OPERATOR(>=) => greater_than_equal__
+
+     GENERIC, PUBLIC :: next => next__, add_integer__
+     GENERIC, PUBLIC :: prev => prev__, subtract_integer__
+     
   END TYPE random_access_iterator
 
 
@@ -81,12 +84,11 @@ MODULE gfcl_iterators
        CLASS(bidirectional_iterator), INTENT(inout)  :: c_this
      END SUBROUTINE op2_bi
 
-     FUNCTION op1_ri(c_this, n) RESULT(itr)
+     SUBROUTINE op1_ri(c_this, n)
        IMPORT :: random_access_iterator
-       CLASS(random_access_iterator), INTENT(in)  :: c_this
-       INTEGER                      , INTENT(in)  :: n
-       CLASS(random_access_iterator), ALLOCATABLE :: itr
-     END FUNCTION op1_ri
+       CLASS(random_access_iterator), INTENT(inout)  :: c_this
+       INTEGER                      , INTENT(in)     :: n
+     END SUBROUTINE op1_ri
 
      FUNCTION op2_ri(n, c_this) RESULT(itr)
        IMPORT :: random_access_iterator
@@ -107,5 +109,36 @@ MODULE gfcl_iterators
        LOGICAL                                   :: b
      END FUNCTION op4_ri
   END INTERFACE
+
+CONTAINS
+
+  SUBROUTINE advance(ForwardIterator,n)
+    CLASS(forward_iterator), INTENT(inout) :: ForwardIterator
+    INTEGER, VALUE                         :: n
+    SELECT TYPE(ForwardIterator)
+    CLASS IS (random_access_iterator)
+       CALL ForwardIterator%next(n)
+    CLASS IS (bidirectional_iterator)
+       IF (n > 0) THEN
+          DO WHILE (n /= 0)
+              CALL ForwardIterator%next()
+             n = n-1
+          END DO
+       ELSE
+          DO WHILE (n /= 0)
+             CALL ForwardIterator%next()
+             n = n+1
+          END DO
+       END IF
+    CLASS IS (forward_iterator)
+       IF (n < 0) ERROR STOP "forward_iterator can only advance forward"
+       DO WHILE (n /= 0)
+          CALL ForwardIterator%next()
+          n = n-1
+       END DO
+    END SELECT
+  END SUBROUTINE advance
+
+
   
 END MODULE gfcl_iterators

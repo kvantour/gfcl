dfa5b72a0a6505d659911c25419f8cc3ede12892
kvantour <15147688+kvantour@users.noreply.github.com>
2017-11-07 17:25:53 +0000

    [LIST] update indentation

--- src/gfl_list.f90
+++ src/gfl_list.f90
@@ -1,45 +1,45 @@
-  !===================================================================
-  !
-  ! PROJECT : Generic Fortran Container Library
-  !
-  ! NAME : linked list
-  !
-  ! GFCL_LIST :
-  !
-  ! Lists are sequence containers that allow constant time insert and
-  ! erase operations anywhere within the sequence, and iteration in
-  ! both directions.
-  !
-  ! List containers are implemented as doubly-linked lists; Doubly
-  ! linked lists can store each of the elements they contain in
-  ! different and unrelated storage locations. The ordering is kept by
-  ! the association to each element of a link to the element preceding
-  ! it and a link to the element following it.
-  !
-  ! They are very similar to forward_list: The main difference being
-  ! that forward_list objects are single-linked lists, and thus they
-  ! can only be iterated forwards, in exchange for being somewhat
-  ! smaller and more efficient.
-  !
-  ! Compared to other base standard sequence containers (array, vector
-  ! and deque), lists perform generally better in inserting,
-  ! extracting and moving elements in any position within the
-  ! container for which an iterator has already been obtained, and
-  ! therefore also in algorithms that make intensive use of these,
-  ! like sorting algorithms.
-  !
-  ! The main drawback of lists and forward_lists compared to these
-  ! other sequence containers is that they lack direct access to the
-  ! elements by their position; For example, to access the sixth
-  ! element in a list one has to iterate from a known position (like
-  ! the beginning or the end) to that position, which takes linear
-  ! time in the distance between these. They also consume some extra
-  ! memory to keep the linking information associated to each element
-  ! (which may be an important factor for large lists of small-sized
-  ! elements).
-  !
-  ! USAGE :
-  !===================================================================
+!===================================================================
+!
+! PROJECT : Generic Fortran Container Library
+!
+! NAME : linked list
+!
+! GFCL_LIST :
+!
+! Lists are sequence containers that allow constant time insert and
+! erase operations anywhere within the sequence, and iteration in
+! both directions.
+!
+! List containers are implemented as doubly-linked lists; Doubly
+! linked lists can store each of the elements they contain in
+! different and unrelated storage locations. The ordering is kept by
+! the association to each element of a link to the element preceding
+! it and a link to the element following it.
+!
+! They are very similar to forward_list: The main difference being
+! that forward_list objects are single-linked lists, and thus they
+! can only be iterated forwards, in exchange for being somewhat
+! smaller and more efficient.
+!
+! Compared to other base standard sequence containers (array, vector
+! and deque), lists perform generally better in inserting,
+! extracting and moving elements in any position within the
+! container for which an iterator has already been obtained, and
+! therefore also in algorithms that make intensive use of these,
+! like sorting algorithms.
+!
+! The main drawback of lists and forward_lists compared to these
+! other sequence containers is that they lack direct access to the
+! elements by their position; For example, to access the sixth
+! element in a list one has to iterate from a known position (like
+! the beginning or the end) to that position, which takes linear
+! time in the distance between these. They also consume some extra
+! memory to keep the linking information associated to each element
+! (which may be an important factor for large lists of small-sized
+! elements).
+!
+! USAGE :
+!===================================================================
 
 MODULE gfcl_list
   !--- USE statements ------------------------------------------------
